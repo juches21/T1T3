0846a73499879a3895589454248cfb84847b1d13
ALUMNADO\juches <draco0221@gmail.com>
2021-12-10 09:35:52 +0100

    prue

--- tests/OperacionesTest.php
+++ tests/OperacionesTest.php
@@ -11,17 +11,17 @@ class TestOperaciones extends TestCase{
 
     public function testSum(){
         $this->assertEquals(7,$this->resultado->sum(2,5));
-        return "Suma ok";
+       
     }
 
     public function testResta(){
         $this->assertEquals(4,$this->resultado->resta(6,2));
-        return "Resta ok";
+    
     }
 
     public function testDivision(){
         $this->assertEquals(5,$this->resultado->division(10,2));
-        return "Division ok";
+        
     }
 
 }
